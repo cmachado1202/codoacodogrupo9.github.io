3067e8e17c9dd4a2204d1dcd37bc1f4f64cf7d29
cmachado1202 <carohandball@hotmail.com>
2023-10-28 21:44:39 -0300

    actualiza footer, primera parte

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-fetch('https://api.ejemplo.com/data')
-    .then(response => response.json())
-    .then(data => {
-        // Haz algo con los datos de la API
-    })
-    .catch(error => {
-        console.error('Error al obtener datos de la API:', error);
-    });
-const slides = document.querySelectorAll('.slide');
-const menuItems = document.querySelectorAll('.menu li a');
+// Espera a que el documento esté listo
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtiene los slides y el contenedor del carrusel
+    const slides = document.querySelectorAll(".slide");
+    const sliderContainer = document.querySelector(".slider-container");
 
-menuItems.forEach((menuItem, index) => {
-    menuItem.addEventListener('click', () => {
-        slides.forEach(slide => (slide.style.display = 'none'));
-        slides[index].style.display = 'block';
-    });
-})
+    // Inicializa el índice del slide actual y el intervalo para el carrusel automático
+    let currentSlide = 0;
+    let interval;
+
+    // Función para cambiar al siguiente slide
+    function nextSlide() {
+        slides[currentSlide].style.display = "none";
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].style.display = "block";
+    }
+
+    // Iniciar el carrusel automáticamente
+    function startAutoSlide() {
+        interval = setInterval(nextSlide, 3000); // Cambia de slide cada 3 segundos (3000 ms)
+    }
+
+    // Detener el carrusel automáticamente
+    function stopAutoSlide() {
+        clearInterval(interval);
+    }
+
+    // Iniciar el carrusel automáticamente al cargar la página
+    startAutoSlide();
+
+    // Detener el carrusel cuando el cursor pasa sobre el contenedor
+    sliderContainer.addEventListener("mouseover", stopAutoSlide);
+
+    // Reanudar el carrusel cuando el cursor sale del contenedor
+    sliderContainer.addEventListener("mouseout", startAutoSlide);
+});
